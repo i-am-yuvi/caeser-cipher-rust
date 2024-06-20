c37e94e55eae017840bf8c57280a8df9f67e5dd1
Yuvraj Chhetri <yuvichh01@gmail.com>
2024-06-20 22:33:49 +0530

    fixes bug

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ fn main() {
     let shift: u8 = 27;
     let mut temp_num: u8;
     let mut temp: char;
-    let str = String::from("spongebob");
+    let str = String::from("zzzzz");
     let mut updated_str = String::new();
 
     for c in str.chars() {
-        temp_num = c as u8 + shift % 26;
-        temp = temp_num as char;
-
+        if c.is_lowercase() {
+            temp_num = (c as u8 - 97) + shift % 26;
+            temp = (temp_num % 26 + 97) as char;
+        } else {
+            temp_num = (c as u8 - 65) + shift % 26;
+            temp = (temp_num % 26 + 65) as char;
+        }
         updated_str += &temp.to_string();
     }
 
